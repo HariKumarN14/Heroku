3688b25edc958c46362fbb65f9478b06cdfc17de
Hari Kumar N <95701865+HariKumarN14@users.noreply.github.com>
2022-03-05 20:18:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -374,9 +374,9 @@ auth= dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server=app.server
 
 app.layout=html.Div([
-    html.Img(src="https://wallpapercave.com/dwp1x/wp7104495.jpg",width="600" ,height="150"),
+    html.Img(src="https://raw.githubusercontent.com/HariKumarN14/Heroku/har-ipl-trial/docs/assets/wp7104495-ipl-logo-wallpapers.jpg",width="600" ,height="150"),
 
-            ([
+            
                      
    html.H1(children='Hello Fan!!!',
            style={"textAlign": "center",
